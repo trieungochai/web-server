110bd68840b3792d47983907727935dec1a16953
Macbook <trieungochai.dev@gmail.com>
2021-01-19 14:24:50 +0700

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -4,7 +4,28 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Trieu Hai'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Trieu Hai'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.'
+  })
+});
 
 app.get('', (req, res) => {
   res.send('<h1>Weather</h1>');
